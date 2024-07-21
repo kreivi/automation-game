981670cc9822c535168f8f01d2a02b97cc0de804
Severi Vidnäs <severi.vidnas@gmail.com>
2024-07-21 18:27:03 +0300

    Project initialized

---
+++ application/script_templates/object/Default.gd
@@ -0,0 +1,14 @@
+# meta-name: Automation Game Default Template
+# meta-description: Default template for Automation Game scripts
+# meta-default: true
+# Automation Game
+# Copyright (C) 2024 Severi Vidnäs
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+## 
+class_name _CLASS_ extends _BASE_
